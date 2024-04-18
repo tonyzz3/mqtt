dff02b8017ccec890ad86395d4310b1999c7610f
tonyzz3 <160858139+tonyzz3@users.noreply.github.com>
2024-04-18 07:48:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def on_message(client, userdata, message):
         
 
 
-broker="broker.mqttdashboard.com"
+broker="broker.hivemq.com"
 port=1883
 client1= paho.Client("ZUP")
 client1.on_message = on_message
